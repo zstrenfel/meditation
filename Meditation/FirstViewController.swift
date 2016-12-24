b55c44282d39a8108445e824c64b490d8c26d76d
Zach Strenfel <zach@Zachs-MacBook-Pro.local>
2016-12-24 09:48:18 +0800

    Initial Commit

---
+++ Meditation/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Meditation
+//
+//  Created by Zach Strenfel on 12/24/16.
+//  Copyright © 2016 Zach Strenfel. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
